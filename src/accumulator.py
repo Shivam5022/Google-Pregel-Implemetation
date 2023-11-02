60a635152b739596f2909662387217f7c3a9e1c4
HemankBajaj <hemankbajaj05@gmail.com>
2023-11-02 18:28:11 +0530

    added example max, sum aggregator

--- src/accumulator.py
+++ src/accumulator.py
@@ -5,6 +5,7 @@ class Aggregator(ABC):
         self.vertices = vertices
         self.aggr = aggr 
 
+    # user implements the method
     @abstractmethod
     def aggregate(self, value):
         raise NotImplementedError 
@@ -13,3 +14,19 @@ class Aggregator(ABC):
         for i in self.vertices:
             self.aggr = self.aggregate(i)
         
+
+class MaxAggregator(Aggregator):
+    def __init__(self, vertices, aggr):
+        super().__init__(vertices, aggr)
+
+    def aggregate(self, value):
+        self.aggr = max(value, self.aggr)
+        return self.aggr
+    
+class SumAggregator(Aggregator):
+    def __init__(self, vertices, aggr):
+        super().__init__(vertices, aggr)
+
+    def aggregate(self, value):
+        self.aggr = value + self.aggr
+        return self.aggr
